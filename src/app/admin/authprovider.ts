bff940f9d7fc6cba66b28b645607e7843222ae18
Gingene <ginx2618@gmail.com>
2025-12-24 09:11:20 +0800

    dev env6

--- src/app/admin/authprovider.ts
+++ src/app/admin/authprovider.ts
@@ -8,7 +8,7 @@ const TOKEN_KEY = "access_token";
 
 // Helper: Get user from localStorage
 const getUser = () => {
-  console.log(process.env.NEXT_PUBLIC_API_URL, 5);
+  console.log(process.env.NEXT_PUBLIC_API_URL, 6);
   const userString = localStorage.getItem(USER_KEY);
   if (!userString) return null;
   try {
